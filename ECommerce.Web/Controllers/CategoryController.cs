11e4f51915054b715f985df94f3204cfa003debc
Aman Kumar Singh <itsamankrsingh@gmail.com>
2025-12-14 12:30:25 +0530

    completed crud

--- ECommerce.Web/Controllers/CategoryController.cs
+++ ECommerce.Web/Controllers/CategoryController.cs
@@ -9,7 +9,7 @@ namespace ECommerce.Web.Controllers
         private readonly ApplicationDbContext mAppDb;
         public CategoryController(ApplicationDbContext _appDb)
         {
-                mAppDb=_appDb;
+            mAppDb = _appDb;
         }
         public IActionResult Index()
         {
@@ -19,7 +19,7 @@ namespace ECommerce.Web.Controllers
 
         public IActionResult Create()
         {
-            
+
             return View();
         }
 
@@ -28,17 +28,85 @@ namespace ECommerce.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                if(obj.Name==obj.DisplayOrder.ToString())
+                if (obj.Name == obj.DisplayOrder.ToString())
                 {
-                    ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name.");
+                    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
                     return View(obj);
                 }
                 mAppDb.Categories.Add(obj);
                 mAppDb.SaveChanges();
-
+                TempData["success"] = "Category created successfully";  
                 return RedirectToAction("Index", "Category");
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = mAppDb.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                mAppDb.Categories.Update(obj);
+                mAppDb.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = mAppDb.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = mAppDb.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            mAppDb.Categories.Remove(category);
+            mAppDb.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
+
+
         }
     }
 }
