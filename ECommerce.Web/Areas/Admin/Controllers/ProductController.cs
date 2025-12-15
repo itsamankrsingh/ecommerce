8e3df9862895c85ff983a35b5e7f41e9aa36b057
Aman Kumar Singh <itsamankrsingh@gmail.com>
2025-12-15 21:43:29 +0530

    added product delete and edit views

--- ECommerce.Web/Areas/Admin/Controllers/ProductController.cs
+++ ECommerce.Web/Areas/Admin/Controllers/ProductController.cs
@@ -34,5 +34,77 @@ namespace ECommerce.Web.Areas.Admin.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Category? category = mAppDb.Categories.Find(id);
+            Product? product = mUnitOfWork.Product.Get(c => c.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                mUnitOfWork.Product.Update(obj);
+                mUnitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Category? category = mAppDb.Categories.Find(id);
+            Product? product = mUnitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            
+            Product? product = mUnitOfWork.Product.Get(c => c.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            
+            mUnitOfWork.Product.Remove(product);
+            mUnitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index", "Product");
+
+
+        }
+
     }
 }
