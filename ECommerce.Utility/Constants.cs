cba71e5ab075259eac893b8ac942dfb0c84d0bb9
Aman Kumar Singh <itsamankrsingh@gmail.com>
2025-12-30 20:11:28 +0530

    added constants

---
+++ ECommerce.Utility/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace ECommerce.Utility
+{
+    public static class Constants
+    {
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusProcessing = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+    }
+}
